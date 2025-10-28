9a72fd3c33cc3a7766ece2c0ce791bcda72473d5
ReZaiden <rezaidensalmani@gmail.com>
2025-10-28 22:40:01 +0330

    Add VideoCallAdmin

--- VideoCall/admin.py
+++ VideoCall/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import VideoCall
 
-# Register your models here.
+class VideoCallAdmin(admin.ModelAdmin):
+    list_display = ('caller', 'receiver', 'call_start_time', 'call_end_time', 'status', 'duration')
+    list_filter = ('status', 'call_start_time')
+    search_fields = ('caller__username', 'receiver__username')
+
+admin.site.register(VideoCall, VideoCallAdmin)
