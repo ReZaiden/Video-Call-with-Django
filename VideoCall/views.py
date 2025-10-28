661cc2f439023b4e6a317040d206049850b06f9e
ReZaiden <rezaidensalmani@gmail.com>
2025-10-28 22:38:00 +0330

    Create simple view for VideoCall

--- VideoCall/views.py
+++ VideoCall/views.py
@@ -1,3 +1,16 @@
+from django.db.models import Q
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from .models import VideoCall
 
-# Create your views here.
+class VideoCallView(View):
+    def get(self, request):
+        user = request.user
+        if user.is_authenticated:
+            call_logs = VideoCall.objects.filter(Q(caller=user) | Q(receiver=user)).order_by('-call_start_time')
+            context = {'call_logs': call_logs}
+
+            return render(request, 'VideoCall/videocall.html', context=context)
+        else:
+            return HttpResponse("Unauthorized", status=401)
