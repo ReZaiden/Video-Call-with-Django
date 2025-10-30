4c525c8d760883707fb5363fd694a4f70ab242fb
ReZaiden <rezaidensalmani@gmail.com>
2025-10-31 00:17:42 +0330

    Config channels in settings

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n$epin7w)fu5*leb+ib$%oan0(uln$t*ijlhqtvqx0c)76o&yy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
     # My apps
     'VideoCall'
 ]
@@ -70,6 +71,18 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'project.wsgi.application'
+
+ASGI_APPLICATION = 'project.asgi.application'
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+            "capacity": 1024,
+            "expiry": 10
+        },
+    },
+}
 
 
 # Database
