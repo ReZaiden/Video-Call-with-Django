ff3e92ea748d7ad0448cb4b47780504ff0eeb1b7
ReZaiden <rezaidensalmani@gmail.com>
2025-10-31 01:11:06 +0330

    Add login url

--- VideoCall/urls.py
+++ VideoCall/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from VideoCall.views import VideoCallView
+from VideoCall.views import VideoCallView, LoginView
 
 urlpatterns = [
-    path('', VideoCallView.as_view()),
+    path('', VideoCallView.as_view(), name='videocall'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
